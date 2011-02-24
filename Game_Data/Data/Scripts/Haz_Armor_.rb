a179ea27a6863690c4c61b6d01ff8f5dd5ad1a60
gegerlan <gegerlan@63d00c09-8322-4db1-ba38-198b11fc15de>
2011-02-24 20:35:31 +0000

    - Updated armor checking script to check for armor fixation blocks
    git-svn-id: http://subversion.assembla.com/svn/aog/trunk@46 63d00c09-8322-4db1-ba38-198b11fc15de

--- Game_Data/Data/Scripts/Haz_Armor_.rb
+++ Game_Data/Data/Scripts/Haz_Armor_.rb
@@ -12,12 +12,16 @@ def player_has_armor?(kind=2) #defaults to check for body armor
   case kind
   when 0
     return true unless $game_party.actors[0].armor1_id == 0
+    return false if $data_actors[$game_party.actors[0].id].armor1_fix == true
   when 1
     return true unless $game_party.actors[0].armor2_id == 0
+    return false if $data_actors[$game_party.actors[0].id].armor2_fix == true
   when 2
     return true unless $game_party.actors[0].armor3_id == 0
+    return false if $data_actors[$game_party.actors[0].id].armor3_fix == true
   when 3
     return true unless $game_party.actors[0].armor4_id == 0
+    return false if $data_actors[$game_party.actors[0].id].armor4_fix == true
   end
   for armor_hash in $game_party.armors
     armor = $data_armors[armor_hash[0]]
