a0a7cca10c397f185fae71568806640d8cb9d194
gegerlan <gegerlan@63d00c09-8322-4db1-ba38-198b11fc15de>
2011-02-24 20:33:11 +0000

    - Disabled CE83 Update Perversion - Updated and re-enabled Perversion script
    git-svn-id: http://subversion.assembla.com/svn/aog/trunk@39 63d00c09-8322-4db1-ba38-198b11fc15de

--- Game_Data/Data/Scripts/Perversion.rb
+++ Game_Data/Data/Scripts/Perversion.rb
@@ -1,19 +1,19 @@
-#module Perversion
-#  VARIABLE = 49
-#end
-#class Game_Battler
-#  alias perv_sp sp=
-#  def sp=(sp)
-#    perv_sp(sp)
-#    if self == $game_actors[1] #If the actor is the first actor (Garnet)
-#      $game_variables[Perversion::VARIABLE] = (@sp * 10) / maxsp 
-#    end
+module Perversion
+  VARIABLE = 49
+end
+class Game_Battler
+  alias perv_sp sp=
+  def sp=(sp)
+    perv_sp(sp)
+    if self == $game_party.actors[0] #If the actor is the leader of the party
+      $game_variables[Perversion::VARIABLE] = (@sp * 100) / @maxsp unless 
+                                                !@sp || !@maxsp || @maxsp == 0
+    end
 #    if self == $game_actors[5] 
 #      $game_variables[Perversion::VARIABLE] = (@sp * 10) / maxsp 
 #    end
 #    if self == $game_actors[6] 
 #      $game_variables[Perversion::VARIABLE] = (@sp * 10) / maxsp 
 #    end
-#  end
-#  
-#end
+  end
+end
