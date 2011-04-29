890b10087122558330cefa1ce7c279ed79310658
gegerlan <gegerlan2@hotmail.com>
2011-04-30 00:56:58 +0100

    Simplified the script checking if there is an armor to equip

--- Game_Data/Data/Scripts/Haz_Armor_.rb
+++ Game_Data/Data/Scripts/Haz_Armor_.rb
@@ -9,6 +9,7 @@ end
 3: accessory
 =end
 def player_has_armor?(kind=2) #defaults to check for body armor
+=begin
   case kind
   when 0
     return true unless $game_party.actors[0].armor1_id == 0
@@ -23,10 +24,18 @@ def player_has_armor?(kind=2) #defaults to check for body armor
     return true unless $game_party.actors[0].armor4_id == 0
     return false if $data_actors[$game_party.actors[0].id].armor4_fix == true
   end
+=end
+  party_actor_id = 0
+  kind_id = kind.to_i + 1
+  armor_id_attr_name = sprintf('armor%d_id', kind_id)
+  armor_fix_attr_name = sprintf('armor%d_fix', kind_id)
+  return true unless $game_party.actors[party_actor_id].send(armor_id_attr_name) == 0
+  return false if $data_actors[$game_party.actors[party_actor_id].id].send(armor_fix_attr_name) == true
+  
   for armor_hash in $game_party.armors
     armor = $data_armors[armor_hash[0]]
     amount = armor_hash[1]
-    if amount != 0 and armor.kind == kind and $game_party.actors[0].equippable?(armor)
+    if amount != 0 and armor.kind == kind and $game_party.actors[party_actor_id].equippable?(armor)
       return true #armor in inventory
     end
   end
