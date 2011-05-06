cd87b18bb79c06e3656bfac8d28e28f6fdd1b4b6
gegerlan <gegerlan2@hotmail.com>
2011-05-06 19:03:32 +0100

    Removed auto game over when the player reaches 0hp through an event or script

---
+++ Game_Data/Data/Scripts/Game_Over_0hp_fix.rb
@@ -0,0 +1,14 @@
+class Game_Party
+  def check_map_slip_damage
+    for actor in @actors
+      if actor.hp > 0 and actor.slip_damage?
+        actor.hp -= [actor.maxhp / 100, 1].max
+        if actor.hp == 0
+          $game_system.se_play($data_system.actor_collapse_se)
+        end
+        $game_screen.start_flash(Color.new(255,0,0,128), 4)
+        #$game_temp.gameover = $game_party.all_dead?
+      end
+    end
+  end
+end
