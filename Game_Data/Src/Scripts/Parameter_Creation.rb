e6f05e65d16f6442e2315619fa3326b3cc9049c5
gegerlan <gegerlan2@hotmail.com>
2011-05-07 17:26:19 +0100

    Fix issues with parameters not being carried over to save games
    Added paramaters to be stored in the game_actor object, so changes to leveling will be existent in save games.
    Parameters is stored in a local table named @parameters.
    All public interfaces remains the same.
    
    Temporary script added to support older save games.

--- Game_Data/Src/Scripts/Parameter_Creation.rb
+++ Game_Data/Src/Scripts/Parameter_Creation.rb
@@ -1,4 +1,10 @@
 class Game_Actor
+  attr_reader   :parameters               # leveling parameters
+  alias parameter_setup setup
+  def setup(id)
+    @parameters = $data_actors[id].parameters
+    parameter_setup(id)
+  end
   #--------------------------------------------------------------------------
   # * Calculate Parameter List
   # 0 : MaxHP
@@ -9,12 +15,91 @@ class Game_Actor
   # 5 : Int
   #--------------------------------------------------------------------------
   def make_parameter_list(parameter=0, min=1, max=99)
-    actor = $data_actors[@actor_id]
-    $data_actors[@actor_id].parameters[parameter, 1] = min
+    @parameters[parameter, 1] = min
     step = (max - min) / 100
     for i in 2..100
-      $data_actors[@actor_id].parameters[parameter, i] = 
+      @parameters[parameter, i] = 
         min + Integer(step*i)
     end
+  end
+  #--------------------------------------------------------------------------
+  # * Get Basic Maximum HP
+  #--------------------------------------------------------------------------
+  def base_maxhp
+    return @parameters[0, @level]
+  end
+  #--------------------------------------------------------------------------
+  # * Get Basic Maximum SP
+  #--------------------------------------------------------------------------
+  def base_maxsp
+    return @parameters[1, @level]
+  end
+    #--------------------------------------------------------------------------
+  # * Get Basic Strength
+  #--------------------------------------------------------------------------
+  def base_str
+    n = @parameters[2, @level]
+    weapon = $data_weapons[@weapon_id]
+    armor1 = $data_armors[@armor1_id]
+    armor2 = $data_armors[@armor2_id]
+    armor3 = $data_armors[@armor3_id]
+    armor4 = $data_armors[@armor4_id]
+    n += weapon != nil ? weapon.str_plus : 0
+    n += armor1 != nil ? armor1.str_plus : 0
+    n += armor2 != nil ? armor2.str_plus : 0
+    n += armor3 != nil ? armor3.str_plus : 0
+    n += armor4 != nil ? armor4.str_plus : 0
+    return [[n, 1].max, 999].min
+  end
+  #--------------------------------------------------------------------------
+  # * Get Basic Dexterity
+  #--------------------------------------------------------------------------
+  def base_dex
+    n = @parameters[3, @level]
+    weapon = $data_weapons[@weapon_id]
+    armor1 = $data_armors[@armor1_id]
+    armor2 = $data_armors[@armor2_id]
+    armor3 = $data_armors[@armor3_id]
+    armor4 = $data_armors[@armor4_id]
+    n += weapon != nil ? weapon.dex_plus : 0
+    n += armor1 != nil ? armor1.dex_plus : 0
+    n += armor2 != nil ? armor2.dex_plus : 0
+    n += armor3 != nil ? armor3.dex_plus : 0
+    n += armor4 != nil ? armor4.dex_plus : 0
+    return [[n, 1].max, 999].min
+  end
+  #--------------------------------------------------------------------------
+  # * Get Basic Agility
+  #--------------------------------------------------------------------------
+  def base_agi
+    n = @parameters[4, @level]
+    weapon = $data_weapons[@weapon_id]
+    armor1 = $data_armors[@armor1_id]
+    armor2 = $data_armors[@armor2_id]
+    armor3 = $data_armors[@armor3_id]
+    armor4 = $data_armors[@armor4_id]
+    n += weapon != nil ? weapon.agi_plus : 0
+    n += armor1 != nil ? armor1.agi_plus : 0
+    n += armor2 != nil ? armor2.agi_plus : 0
+    n += armor3 != nil ? armor3.agi_plus : 0
+    n += armor4 != nil ? armor4.agi_plus : 0
+    return [[n, 1].max, 999].min
+  end
+  #--------------------------------------------------------------------------
+  # * Get Basic Intelligence
+  #--------------------------------------------------------------------------
+  def base_int
+    n = @parameters[5, @level]
+    weapon = $data_weapons[@weapon_id]
+    armor1 = $data_armors[@armor1_id]
+    armor2 = $data_armors[@armor2_id]
+    armor3 = $data_armors[@armor3_id]
+    armor4 = $data_armors[@armor4_id]
+    n += weapon != nil ? weapon.int_plus : 0
+    n += armor1 != nil ? armor1.int_plus : 0
+    n += armor2 != nil ? armor2.int_plus : 0
+    n += armor3 != nil ? armor3.int_plus : 0
+    n += armor4 != nil ? armor4.int_plus : 0
+    return [[n, 1].max, 999].min
   end
 end
