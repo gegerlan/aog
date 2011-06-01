bb8a3496f2417f67723a0980a8b3324ee97957f9
gegerlan <gegerlan2@hotmail.com>
2011-06-01 10:38:58 +0100

    Added support for priority among character sprite layers.
    Instead of setting the character bitmap, as previously done, the character base bitmap is added to a hash with all the other sprites used to create the character. The order of the sprites used is based on the direction of the character, and is currently hard-coded.

--- Game_Data/Src/Scripts/Sprite+equipment.rb
+++ Game_Data/Src/Scripts/Sprite+equipment.rb
@@ -23,67 +23,97 @@ class Sprite_Character < RPG::Sprite
   #--------------------------------------------------------------------------
   # * Frame Update
   #--------------------------------------------------------------------------
+  
+  alias sprite_update update
   def update
     super
+    if @character != $game_player
+      sprite_update
+    else
+      player_update
+    end
+  end
+  def player_update
+    actor = $game_party.actors[0]
     # If tile ID, file name, or hue are different from current ones
     if @tile_id != @character.tile_id or
        @character_name != @character.character_name or
-       @character_hue != @character.character_hue
+       @character_hue != @character.character_hue or
+       @character_armor1 != actor.armor1_id or
+       @character_armor2 != actor.armor2_id or
+       @character_armor3 != actor.armor3_id or
+       @character_weapon != actor.weapon_id
+      
       # Remember tile ID, file name, and hue
       @tile_id = @character.tile_id
       @character_name = @character.character_name
       @character_hue = @character.character_hue
-      # If tile ID value is valid
-      if @tile_id >= 384
-        self.bitmap = RPG::Cache.tile($game_map.tileset_name,
-          @tile_id, @character.character_hue)         
-        self.src_rect.set(0, 0, 32, 32)
-        self.ox = 16
-        self.oy = 32
-      # If tile ID value is invalid
-      else
-        self.bitmap = RPG::Cache.character(@character.character_name,
-          @character.character_hue)
-        @cw = self.bitmap.width / 4
-        @ch = self.bitmap.height / 4
-        self.ox = @cw / 2
-        self.oy = @ch
-      end
-    end
-    
-    # Add collar, shield and/or sword to sprite
-    if @character == $game_player && 
-        $data_classes[$game_party.actors[0].class_id].name != "Dog" #FIXME!
+      
+      # Remember ID of equipment
+      @character_armor1 = actor.armor1_id
+      @character_armor2 = actor.armor2_id
+      @character_armor3 = actor.armor3_id
+      @character_weapon = actor.weapon_id
       begin
-        bitmap = []
-        if $game_party.actors[0].armor2_id != 0
-          bitmap.push(
+        bitmap = Hash.new
+        
+          # If tile ID value is valid
+        if @tile_id >= 384
+          bitmap["base"] << RPG::Cache.tile($game_map.tileset_name,
+            @tile_id, @character.character_hue)
+          
+          self.src_rect.set(0, 0, 32, 32)
+          self.ox = 16
+          self.oy = 32
+        # If tile ID value is invalid
+        else
+          char_bitmap = RPG::Cache.character(@character.character_name,
+            @character.character_hue)
+          bitmap["base"] = char_bitmap
+          @cw = char_bitmap.width / 4
+          @ch = char_bitmap.height / 4
+          self.bitmap = Bitmap.new(char_bitmap.width, char_bitmap.height)
+          self.ox = @cw / 2
+          self.oy = @ch
+        end
+        
+        
+        if @character_armor2 != 0
+          bitmap["accessories"] =
             RPG::Cache.character("Garnet Slave Collar.png", 
                 @character.character_hue)
-          )
         end
-        if $game_party.actors[0].weapon_id != 0
-          bitmap.push( 
+        if @character_weapon != 0
+          bitmap["weapon"] =
             RPG::Cache.character("Garnet Sword.png", 
-                @character.character_hue)
-          )
+              @character.character_hue)
         end
-        if $game_party.actors[0].armor1_id != 0
-          bitmap.push(
+        if @character_armor1 != 0
+          bitmap["shield"] =
             RPG::Cache.character("Garnet Shield.png", 
                 @character.character_hue)
-          )
         end
-        bitmap.each do |layer| 
-          rect = Rect.new(0,0,layer.width,layer.height)
-          self.bitmap.blt(0,0,layer,rect)
+        
+        draw_order = 
+        {
+          2 => ["base", "accessories", "shield", "weapon"], #facing down
+          4 => ["weapon", "base", "accessories", "shield"], #facing left (shield in left hand)
+          6 => ["shield", "base", "accessories", "weapon"], #facing right (weapon in right hand)
+          8 => ["shield", "weapon", "accessories", "base"]  #facing up
+        }
+
+        draw_order[@character.direction].each do |layer_name|
+          layer = bitmap[layer_name]
+          if layer != nil
+            rect = Rect.new(0,0,layer.width,layer.height)
+            self.bitmap.blt(0,0,layer,rect)
+          end
         end
       rescue Exception => e
         print e if $DEBUG
       end
     end
-    
-    
+
     # Set visible situation
     self.visible = (not @character.transparent)
     # If graphic is character
@@ -109,4 +139,3 @@ class Sprite_Character < RPG::Sprite
     end
   end
 end
-# Update sprite as the equipment for the/any actor change
