fe907ac451a13a336124f9e50ba6ee915fd9da3f
gegerlan <gegerlan2@hotmail.com>
2011-06-06 22:15:12 +0100

    Simple alpha render layer for light.

--- Game_Data/Src/Scripts/Light.rb
+++ Game_Data/Src/Scripts/Light.rb
@@ -14,110 +14,47 @@ end
 class Spriteset_Map
   alias light_initialize initialize
   def initialize
-    @darkness = Sprite.new(@viewport1)
-    canvas = Bitmap.new(640,480)
     
-    ambiance = 200.0
+    @light_viewport = Viewport.new(0,0,640,480)
+    @light_viewport.z = 500
     
-    canvas.fill_rect(0, 0, canvas.width, canvas.height, Color.new(ambiance,ambiance,ambiance,128)) 
-    @darkness.bitmap = canvas
-
-    @darkness.blend_type = 2
-    @darkness.z = 101
+    @light_alpha = Sprite.new(@light_viewport)
+    @light_alpha.bitmap = Bitmap.new(640, 480)
     
-    #canvas.fill_rect(0, 0, canvas.width, canvas.height, Color.new(ambiance,ambiance,ambiance,128)) 
-    @darkness2 = Sprite.new(@viewport1)
-    @darkness2.bitmap = canvas.clone
-
-    @darkness2.blend_type = 2
-    @darkness2.z = 103
+    @light_alpha.blend_type = 2
+    #@light_alpha.blend_type = 0
+    
+    @light_beam_alpha = make_light(100, 1)
+    
+    light_initialize
     
     
+    @light_alpha.bitmap.fill_rect(
+      0, 
+      0, 
+      @light_alpha.bitmap.width, 
+      @light_alpha.bitmap.height, 
+      Color.new(255,255,255)
+    )
+    $game_map.events.each do |idx, event|
+      @light_alpha.bitmap.blt(
+        event.screen_x - @light_beam_alpha.width / 2, 
+        event.screen_y - @light_beam_alpha.height / 2, 
+        @light_beam_alpha,
+        Rect.new(0, 0, @light_beam_alpha.width, @light_beam_alpha.height)
+      )
+    end    
     
-    @light = {}
     
-    $game_map.events.each do |k, event| 
-      r,g,b = event.name.scan(/\\c\[(\d+),(\d+),(\d+)\]/)[0]
-      
-      reach = event.name.scan(/\\r\[(\d+)\]/)[0][0].to_f
-      reach_modifier = 1.2
-      strength = event.name.scan(/\\s\[([\d\.]+)\]/)[0][0].to_f
-      strength_modifier = 0.8
-      
-      ambiance_modifier = ambiance / 255.0
-      
-      reach *= reach_modifier
-      strength *= strength_modifier * ambiance_modifier
-      
-      @light[event] = get_light_sprites(reach, strength, r, g, b)
-      @light[event].each do |sprite|
-        sprite.x = event.screen_x - reach / 2
-        sprite.y = event.screen_y - reach / 2
-      end
-    end
-    
-    @light[$game_player] = get_light_sprites(180, 0.4, 0, 0, 100)
-
-    light_initialize 
   end
+  
   alias light_update update
   def update
-    @light.each do |event, sprites|
-      sprites.each do |sprite|
-        sprite.x = event.screen_x - sprite.bitmap.width / 2
-        sprite.y = event.screen_y - sprite.bitmap.height / 2 - 16
-      end
-    end
+
+    @light_viewport.update
     light_update
   end
-  def get_light_sprites(reach, strength, red, green, blue)
-    $light_cache = {} if $light_cache == nil 
-    
-    if $light_cache[[reach, strength]] == nil
-      $light_cache[[reach, strength]] = make_light(reach, strength)
-    end
-    
-    canvas = $light_cache[[reach, strength]]
-    canvas_color = make_light_alpha(reach, strength.to_f*0.8)
-    canvas_color_2 = make_light_alpha(reach.to_f*0.5, strength)
-    
-    alpha = Sprite.new(@viewport1)
-    color = Sprite.new(@viewport1)
-    
-    alpha.bitmap = canvas
-    alpha.blend_type = 1
-    alpha.z = 100
-    
-    alpha2 = Sprite.new(@viewport1)
-    alpha2.bitmap = canvas
-    alpha2.blend_type = 1
-    alpha2.z = 102
-    
-    r = red.to_i ^ 255
-    g = green.to_i ^ 255
-    b = blue.to_i ^ 255
-    
-    color.bitmap = canvas_color
-    color.color = Color.new(
-      r,
-      g,
-      b
-    )
-    color.z = 99
-    color.blend_type = 2
-    
-    color2 = Sprite.new(@viewport1)
-    color2.bitmap = canvas_color_2
-    color2.color = Color.new(
-      red.to_f,
-      green.to_f,
-      blue.to_f
-    )
-    color2.z = 104
-    color2.blend_type = 1
-    
-    return [alpha, alpha2, color, color2]
-  end
+
   
   def make_light(reach, strength)
     diameter = reach * 2
@@ -127,33 +64,12 @@ class Spriteset_Map
       for y in 0..diameter do
         dist_sqr = (reach - x)**2 + (reach - y)**2
         if dist_sqr <= reach_sqr
-          alpha = 255.0 * dist_sqr.to_f ** (-dist_sqr.to_f / (reach_sqr.to_f + dist_sqr.to_f))
-          alpha *= strength
-          #alpha += 200
-          #alpha = 255
-          alpha_color = Color.new(alpha, alpha, alpha)
+          alpha = 255.0 * (dist_sqr.to_f / reach_sqr.to_f)
+          alpha_color = Color.new(0, 0, 0, 255 - alpha)
           canvas.set_pixel(x, y, alpha_color)
         end
       end
     end
     return canvas
   end
-  def make_light_alpha(reach, strength)
-    diameter = reach * 2
-    canvas = Bitmap.new(diameter, diameter)
-    reach_sqr = reach ** 2
-    for x in 0..diameter do
-      for y in 0..diameter do
-        dist_sqr = (reach - x)**2 + (reach - y)**2
-        if dist_sqr <= reach_sqr
-          alpha = 255.0 * dist_sqr.to_f ** (-dist_sqr.to_f / (reach_sqr.to_f + dist_sqr.to_f))
-          alpha *= strength
-          alpha_color = Color.new(alpha, alpha, alpha, alpha)
-          canvas.set_pixel(x, y, alpha_color)
-        end
-      end
-    end
-    return canvas
-  end
-  
 end
