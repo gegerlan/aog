ccc8522fbaf732d34f3f10401e818d3a0557eaa6
gegerlan <gegerlan2@hotmail.com>
2011-06-07 21:57:15 +0100

    Added support for custom color for lights. Added commands in event names.
    \red[<0..255>] sets how much red is transmitted from the source. Default 0.
    \green[<0..255>] sets how much green is transmitted from the source. Default 0.
    \blue[<0..255>] sets how much blue is transmitted from the source. Default 0.
    \strength[<0..1>] sets the strength of the light source (alpha opacity). Default 1.
    
    By providing no color, the ground will be visible, but there are no visible light being cast (i.e. a hole in the tint).

--- Game_Data/Src/Scripts/Light.rb
+++ Game_Data/Src/Scripts/Light.rb
@@ -1,3 +1,10 @@
+#TODO: Update layers only on changes to light source
+#TODO: Bind light source only to sepecific events
+#TODO: Fix blending (z-layer for tiles?)
+#TODO: Add light center/source (focal point)
+#TODO: Change ranges for light circles (possibly blt-stretch)
+#TODO: Add pulsing effect (opacity/size) for light source
+
 class Game_Event < Game_Character
   def name
     return @event.name
@@ -22,27 +29,23 @@ class Spriteset_Map
     @light_alpha.bitmap = Bitmap.new(640, 480)
     @light_alpha.blend_type = 2
     
-=begin
-    @green_alpha = Sprite.new(@light_viewport)
-    @green_alpha.bitmap = Bitmap.new(640, 480)
-    @green_alpha.blend_type = 2
-    #@green_alpha.color = Color.new(255, 0, 255)
-    @green_alpha.opacity = 255
+    @light_alpha.z  = 1
+    @color_layer = {}
+    {
+      'red'   => Tone.new(   0, -255, -255), 
+      'green' => Tone.new(-255,    0, -255), 
+      'blue'  => Tone.new(-255, -255,    0)
+    }.each do |color, tone|
+      color_alpha = Sprite.new(@light_viewport)
+      color_alpha.bitmap = Bitmap.new(640, 480)
+      color_alpha.tone = tone
+      color_alpha.blend_type = 1
+      color_alpha.z = 0
+      @color_layer[color] = color_alpha
+    end  
 
-    
-
-    @blue_alpha = Sprite.new(@light_viewport)
-    @blue_alpha.bitmap = @light_alpha.bitmap
-    @blue_alpha.blend_type = 2
-    @blue_alpha.color = Color.new(255, 255, 0)
-    
-    @red_alpha = Sprite.new(@light_viewport)
-    @red_alpha.bitmap = @light_alpha.bitmap
-    @red_alpha.blend_type = 2
-    @red_alpha.color = Color.new(0, 255, 255)
-=end    
-
-    @light_beam_alpha = make_light(100, 1)
+    @light_beam_alpha = make_light(100, 1, Color.new(0, 0, 0))
+    @color_beam_alpha = make_light(100, 1)
     
     $light = Color.new(255, 255, 255, 0)
     
@@ -59,30 +62,68 @@ class Spriteset_Map
       @light_alpha.bitmap.width, 
       @light_alpha.bitmap.height, 
       Color.new(
-        $light.red,
-        $light.green,
-        $light.blue,
-        $light.alpha
+        255,
+        255,
+        255,
+        255
       )
     )
-
+    @color_layer.each do |name, layer|
+      layer.bitmap.fill_rect(
+        0, 
+        0, 
+        @light_alpha.bitmap.width, 
+        @light_alpha.bitmap.height, 
+        Color.new(
+          0,
+          0,
+          0,
+          255
+        )
+      )
+      layer.opacity = $light.alpha / 2
+    end
+    
+    
+    @light_alpha.opacity = $light.alpha
+    
     range = @light_beam_alpha.width / 2
 
     $game_map.events.each do |idx, event|
       next if event.screen_x < -range || event.screen_x > range + 640 || event.screen_y < -range || event.screen_y > range + 480
+      strength_match = event.name.scan(/\\strength\[([\d\.]+)\]/)
+      strength = strength_match[0] == nil ? 1.0 : strength_match[0][0].to_f
       @light_alpha.bitmap.blt(
         event.screen_x - range,
         event.screen_y - range, 
         @light_beam_alpha,
-        Rect.new(0, 0, @light_beam_alpha.width, @light_beam_alpha.height)
+        Rect.new(0, 0, @light_beam_alpha.width, @light_beam_alpha.height),
+        255.0 * strength
       )
+      @color_layer.each do |name, layer|
+        layer_match = event.name.scan(/\\#{Regexp.quote(name)}\[(\d+)\]/)
+        next if layer_match[0] == nil
+        opacity = layer_match[0][0].to_i# if match.is_a?(Regex)
+        layer.bitmap.blt(
+          event.screen_x - range,
+          event.screen_y - range, 
+          @color_beam_alpha,
+          Rect.new(
+            0, 
+            0,
+            @color_beam_alpha.width,
+            @color_beam_alpha.height
+          ),
+          opacity
+        )
+      end
     end
     
     @light_viewport.update
     light_update
   end
   
-  def make_light(reach, strength)
+  def make_light(reach, strength, color = Color.new(255, 255, 255))
     diameter = reach * 2
     canvas = Bitmap.new(diameter, diameter)
     reach_sqr = reach ** 2
@@ -91,7 +132,9 @@ class Spriteset_Map
         dist_sqr = (reach - x)**2 + (reach - y)**2
         if dist_sqr <= reach_sqr
           alpha = 255.0 * (dist_sqr.to_f / reach_sqr.to_f)
-          alpha_color = Color.new(0, 0, 0, 220 - alpha)
+          alpha = 255.0 - alpha 
+          color.alpha = alpha
+          alpha_color = color
           canvas.set_pixel(x, y, alpha_color)
         end
       end
