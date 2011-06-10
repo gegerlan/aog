66bf2b670a1726bd115d04c247837f4fba6cf337
gegerlan <gegerlan2@hotmail.com>
2011-06-10 12:51:13 +0100

    Added proper alpha, glow, and ambiance.
    Blending still remains somewhat of an issue.

--- Game_Data/Src/Scripts/Light.rb
+++ Game_Data/Src/Scripts/Light.rb
@@ -20,36 +20,59 @@ class Light_Source
     @range = range
     @strength = strength
     @tone = Tone.new(red, green, blue, alpha)
-    @layer = Light_Manager.get_radiant(range, strength)
+    @black_ambiance_layer = Light_Manager.get_radiant(range, strength, Color.new(0,0,0))
+    @white_ambiance_layer = Light_Manager.get_radiant(range, strength, Color.new(255,255,255))
+    @white_beam_layer = Light_Manager.get_radiant(range, strength, Color.new(255,255,255), false)
   end
-  def draw(bitmap, opacity)
-    self_center_x = self.x + self.width / 2
-    self_center_y = self.y + self.height / 2
+  def draw(bitmap, layer, opacity)
+    self_center_x = self.x - self.width / 2
+    self_center_y = self.y - self.height / 2
 
-    layer_width = @layer.width
-    layer_height = @layer.height
-    layer_center_x = self_center_x - @layer.width / 2
-    layer_center_y = self_center_y - @layer.height / 2
+    layer_width = layer.width
+    layer_height = layer.height
+    layer_center_x = self_center_x #- @layer.width / 2
+    layer_center_y = self_center_y #- @layer.height / 2
     layer_rectangle = Rect.new(0, 0, width, height)
     bitmap.blt(
       layer_center_x,
       layer_center_y,
-      @layer,
+      layer,
       layer_rectangle, #This can be cashed!
       opacity
     )
   end
-  def draw_alpha(bitmap)
-    draw(bitmap, @tone.gray) if @tone.gray != 0
+  def draw_alpha_ambiance(bitmap)
+    draw(bitmap, @black_ambiance_layer, @tone.gray) if @tone.gray != 0
   end
-  def draw_red(bitmap)
-    draw(bitmap, @tone.red) if @tone.red != 0
+  def draw_red_ambiance(bitmap)
+    draw(bitmap, @white_ambiance_layer, @tone.red) if @tone.red != 0
   end
-  def draw_green(bitmap)
-    draw(bitmap, @tone.green) if @tone.green != 0
+  def draw_green_ambiance(bitmap)
+    draw(bitmap, @white_ambiance_layer, @tone.green) if @tone.green != 0
   end
-  def draw_blue(bitmap)
-    draw(bitmap, @tone.blue) if @tone.blue != 0
+  def draw_blue_ambiance(bitmap)
+    draw(bitmap, @white_ambiance_layer, @tone.blue) if @tone.blue != 0
+  end
+  def draw_red_beam(bitmap)
+    draw(bitmap, @white_beam_layer, @tone.red) if @tone.red != 0
+  end
+  def draw_green_beam(bitmap)
+    draw(bitmap, @white_beam_layer, @tone.green) if @tone.green != 0
+  end
+  def draw_blue_beam(bitmap)
+    draw(bitmap, @white_beam_layer, @tone.blue) if @tone.blue != 0
+  end
+  def updates_green?
+    return @tone.green != 0
+  end
+  def updates_red?
+    return @tone.red != 0
+  end
+  def updates_blue?
+    return @tone.blue != 0
+  end
+  def updates_alpha?
+    return @tone.gray != 0
   end
   def x 
     return @event.screen_x
@@ -66,26 +89,36 @@ class Light_Source
 end
 
 class Light_Manager
-  def self.get_radiant(range, strength)
+  def self.get_radiant(range, strength, color, linear = true)
+    range = range.to_f
+    strength = strength.to_f
     @@radiant ||= {}
-    if @@radiant[[range, strength]] == nil
+    if @@radiant[[range, strength, color, linear]] == nil
       diameter = range * 2
-      canvas = Bitmap.new(diameter, diameter)
       range_sqr = range ** 2
+      canvas = Bitmap.new(diameter, diameter)
       for x in 0..diameter do
         for y in 0..diameter do
           dist_sqr = (range - x)**2 + (range - y)**2
           if dist_sqr <= range_sqr
-            alpha = 255.0 * strength * (dist_sqr.to_f / range_sqr.to_f) #dist_sqr.to_f ** (-dist_sqr.to_f / (reach_sqr.to_f + dist_sqr.to_f)) #(dist_sqr.to_f / reach_sqr.to_f)
-            alpha = 255.0 - alpha 
-            alpha_color = Color.new(alpha,alpha,alpha)
-            canvas.set_pixel(x, y, alpha_color)
+            if (linear)
+              alpha = 255.0 * strength * (dist_sqr.to_f / range_sqr.to_f) #dist_sqr.to_f ** (-dist_sqr.to_f / (reach_sqr.to_f + dist_sqr.to_f)) #(dist_sqr.to_f / reach_sqr.to_f)
+              alpha = 255.0 - alpha 
+            else
+              alpha = 255.0 * strength * dist_sqr.to_f ** (-dist_sqr.to_f / (range_sqr.to_f + dist_sqr.to_f))
+            end
+            color.alpha = alpha
+            #color.red = alpha
+            #color.green = alpha
+            #color.blue = alpha
+            
+            canvas.set_pixel(x, y, color)
           end
         end
       end
-      @@radiant[[range, strength]] = canvas
+      @@radiant[[range, strength, color, linear]] = canvas
     end
-    return @@radiant[[range, strength]]
+    return @@radiant[[range, strength, color, linear]]
   end
 end
 
@@ -95,9 +128,9 @@ class Alpha_Layer < Sprite
     @clear_color = clear_color
     viewport_rect = viewport.rect
     self.bitmap = Bitmap.new(viewport_rect.width, viewport_rect.height)
-    clear(clear_color)
+    clear
   end
-  def clear(color)
+  def clear
     self.bitmap.fill_rect(
       0,
       0,
@@ -116,91 +149,89 @@ end
 #==============================================================================
 
 class Spriteset_Map
-=begin
-  def create_alpha_layer(
-    viewport, 
-    blend_type = 0, 
-    z_index = 0, 
-    tone = Tone.new(0,0,0,0)
-  )
-    layer = Sprite.new(viewport)
-    viewport_rect = viewport.rect
-    layer.bitmap = Bitmap.new(
-      viewport_rect.width, 
-      viewport_rect.height
-    )
-    layer.tone = tone
-    layer.blend_type = blend_type
-    layer.z = z_index
-    return layer
-  end
-=end
   alias light_initialize initialize
   def initialize
     @event_last_position = {}
     
-    $light = Tone.new(255, 255, 255, 255)
+    $light = Tone.new(255, 255, 255, 100)
     
     @light_viewport = Viewport.new(0,0,640,480)
     @light_viewport.z = 50
     
-    @alpha_layer = Alpha_Layer.new(@light_viewport, Color.new(0,0,0))
+    @alpha_layer = Alpha_Layer.new(@light_viewport, Color.new(255,255,255))
     @alpha_layer.blend_type = 2
-    @alpha_layer.z = 1
-    @alpha_layer.tone = Tone.new(-255, -255, -255)
+    @alpha_layer.z = 2
+    #@alpha_layer.tone = Tone.new(255, 255, 255)
     
     @red_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
     @red_layer.blend_type = 2
     @red_layer.tone = Tone.new(-255, 0, 0)
-    @red_layer.z = 0
+    @red_layer.z = 1
     
     @green_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
     @green_layer.blend_type = 2
     @green_layer.tone = Tone.new(0, -255, 0)
-    @green_layer.z = 0
+    @green_layer.z = 1
     
     @blue_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
     @blue_layer.blend_type = 2
     @blue_layer.tone = Tone.new(0, 0, -255)
-    @blue_layer.z = 0
+    @blue_layer.z = 1
+    
+    @red_beam_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
+    @red_beam_layer.blend_type = 1
+    @red_beam_layer.tone = Tone.new(0, -255, -255)
+    @red_beam_layer.z = 3
+    
+    @green_beam_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
+    @green_beam_layer.blend_type = 1
+    @green_beam_layer.tone = Tone.new(-255, 0, -255)
+    @green_beam_layer.z = 3
+    
+    @blue_beam_layer = Alpha_Layer.new(@light_viewport, Color.new(0, 0, 0))
+    @blue_beam_layer.blend_type = 1
+    @blue_beam_layer.tone = Tone.new(-255, -255, 0)
+    @blue_beam_layer.z = 3
 
-    @light_source = []
-    @command = {
-      "range" => 50.0,
-      "strength" => 1.0,
-      "red" => 255.0,
-      "green" => 255.0,
-      "blue" => 255.0,
-      "alpha" => 255.0
-    }
-    $game_map.events.each do |index, event|
+    @light_source = get_light_source($game_map.events)
+    light_initialize
+  end
+  
+  def get_light_source(events)
+    command_list = [
+      ["range", 25.0],
+      ["strength", 1.0],
+      ["red", 0.0],
+      ["green", 0.0],
+      ["blue", 0.0],
+      ["alpha", 255.0]
+    ]
+    light_source = []
+    events.each do |index, event|
       event_name = event.name
       if event_name =~ /\\light\b/i
         param = [event]
-        @command.each do |command, default|
+        command_list.each do |command|
+          command, default = command
           cmd_search = event_name.scan(/\\#{Regexp.quote(command)}\[([\d\.]+)\]/i)
-          if cmd_search[0] && cmd_search[0][0]
+          if cmd_search[0] != nil && cmd_search[0][0] != nil
             param << cmd_search[0][0].to_f
           else
             param << default  
           end
         end
-        @light_source << Light_Source.new(*param)
+        light_source << Light_Source.new(*param)
       end
     end
-    light_initialize
+    return light_source
   end
   
-  alias light_update update
-  def update
-    # Update tint level (reflects time of day)
-    @alpha_layer.opacity = 100 #$light.gray
-    # Since we don't use ambient light we need to fake ambiance light during day
-    #@red_layer.tone.gray   = $light.gray / 2
-    #@green_layer.tone.gray = $light.gray / 2
-    #@blue_layer.tone.gray  = $light.gray / 2
-    
-    @light_source.each do |light_source|
+  def get_modified_light_source(light_sources)
+    red_draw_stack = []
+    blue_draw_stack = []
+    green_draw_stack = []
+    alpha_draw_stack = []
+    light_sources.each do |light_source|
       light_source_x = light_source.x
       light_source_y = light_source.y
       visible = true #light_source.event.visible
@@ -216,124 +247,89 @@ class Spriteset_Map
                          @event_last_position[light_source] != 
                                [light_source_x, light_source_y]
           if new_position
-            #print "draw!"
-            light_source.draw_alpha(@alpha_layer.bitmap)
-            light_source.draw_red(@red_layer.bitmap)
-            light_source.draw_green(@green_layer.bitmap)
-            light_source.draw_blue(@blue_layer.bitmap)
+            if light_source.updates_alpha? 
+              alpha_draw_stack << light_source
+            end            
+            if light_source.updates_red?
+              red_draw_stack << light_source
+            end
+            if light_source.updates_green?
+              green_draw_stack << light_source
+            end
+            if light_source.updates_blue?
+              blue_draw_stack << light_source
+            end
           end
           @event_last_position[light_source] = [light_source_x, light_source_y]
         end #eif on_screen
       end #eif visible
     end #eeach light_source
+    return [red_draw_stack, green_draw_stack, blue_draw_stack, alpha_draw_stack]
+  end
+  
+  alias light_update update
+  def update
     
-=begin
-    @color_layer.each do |name, layer|
-      layer.opacity = $light.tone.gray / 2
-    end
-    @beam_layer.each do |name, layer|
-      layer.opacity = $light.tone.gray
-    end
-    
-    @light_alpha.opacity = $light.tone.gray
-    
-    range = @light_beam_alpha.width / 2
-    
-    @old_position ||= {}
-    
-    layer_update = []
-    
-    $game_map.events.each do |idx, event|
-      next if event.screen_x < -range || event.screen_x > range + 640 || event.screen_y < -range || event.screen_y > range + 480
-      if @old_position[event] != nil
-        next if @old_position[event]["x"] == event.screen_x && @old_position[event]["y"] == event.screen_y
-      end
-      @old_position[event] = { 'x' => event.screen_x, 'y' => event.screen_y }
-      
-      if not layer_update.include?(@light_alpha.bitmap)
-        bitmap.fill_rect(0, 0, width, height, Color.new(255,255,255))
-        layer_update << @light_alpha.bitmap
-      end
-      
-      strength_match = event.name.scan(/\\strength\[([\d\.]+)\]/)
-      strength = strength_match[0] == nil ? 1.0 : strength_match[0][0].to_f
-      @light_alpha.bitmap.blt(
-        event.screen_x - @light_beam_alpha.width / 2,
-        event.screen_y - @light_beam_alpha.width / 2 - 16, 
-        @light_beam_alpha,
-        Rect.new(0, 0, @light_beam_alpha.width, @light_beam_alpha.height),
-        255.0 * strength
-      )
-      @color_layer.each do |name, layer|
-        layer_match = event.name.scan(/\\#{Regexp.quote(name)}\[(\d+)\]/)
-        next if layer_match[0] == nil
-        opacity = layer_match[0][0].to_i# if match.is_a?(Regex)
-        
-        if not layer_update.include?(layer.bitmap)
-          bitmap.fill_rect(0, 0, width, height, Color.new(0,0,0))
-          layer_update << layer.bitmap
-        end
-        
-        layer.bitmap.blt(
-          event.screen_x - @color_envrionment_alpha.width / 2,
-          event.screen_y - @color_envrionment_alpha.width / 2 - 16, 
-          @color_envrionment_alpha,
-          Rect.new(
-            0, 
-            0,
-            @color_envrionment_alpha.width,
-            @color_envrionment_alpha.height
-          ),
-          opacity * strength
-        )
-      end
-      @beam_layer.each do |name, layer|
-        layer_match = event.name.scan(/\\#{Regexp.quote(name)}\[(\d+)\]/)
-        next if layer_match[0] == nil
-        opacity = layer_match[0][0].to_i# if match.is_a?(Regex)
-        
-        if not layer_update.include?(layer.bitmap)
-          bitmap.fill_rect(0, 0, width, height, Color.new(0,0,0))
-          layer_update << layer.bitmap
-        end
-        
-        layer.bitmap.blt(
-          event.screen_x - @color_beam_alpha.width / 2,
-          event.screen_y - @color_beam_alpha.width / 2 - 16, 
-          @color_beam_alpha,
-          Rect.new(
-            0, 
-            0,
-            @color_beam_alpha.width,
-            @color_beam_alpha.height
-          ),
-          opacity * strength
-        )
-      end
-    end
+    red_draw_stack,
+        blue_draw_stack,
+        green_draw_stack,
+        alpha_draw_stack = get_modified_light_source(@light_source)
+=begin    
+    update_layer([@alpha_layer], alpha_draw_stack) if not alpha_draw_stack.empty?
+    update_layer([@red_layer, @red_beam_layer], alpha_draw_stack) if not red_draw_stack.empty?
+    update_layer([@green_layer, @green_beam_layer], alpha_draw_stack) if not green_draw_stack.empty?
+    update_layer([@blue_layer, @blue_beam_layer], alpha_draw_stack) if not blue_draw_stack.empty?
 =end
+
+    if not alpha_draw_stack.empty?
+      @alpha_layer.clear
+      @light_source.each do |light_source|
+        light_source.draw_alpha_ambiance(@alpha_layer.bitmap)
+      end
+    end
+    if not red_draw_stack.empty?
+      @red_layer.clear
+      @red_beam_layer.clear
+      @light_source.each do |light_source|
+        light_source.draw_red_ambiance(@red_layer.bitmap)
+        light_source.draw_red_beam(@red_beam_layer.bitmap)
+      end
+    end
+    if not green_draw_stack.empty?
+      @green_layer.clear
+      @green_beam_layer.clear
+      @light_source.each do |light_source|
+        light_source.draw_green_ambiance(@green_layer.bitmap)
+        light_source.draw_green_beam(@green_beam_layer.bitmap)
+      end
+    end
+    if not blue_draw_stack.empty?
+      @blue_layer.clear
+      @blue_beam_layer.clear
+      @light_source.each do |light_source|
+        light_source.draw_blue_ambiance(@blue_layer.bitmap)
+        light_source.draw_blue_beam(@blue_beam_layer.bitmap)
+      end
+    end
     
     @light_viewport.update
     light_update
   end
 =begin
-  def make_light(reach, strength, color = Color.new(255, 255, 255))
-    diameter = reach * 2
-    canvas = Bitmap.new(diameter, diameter)
-    reach_sqr = reach ** 2
-    for x in 0..diameter do
-      for y in 0..diameter do
-        dist_sqr = (reach - x)**2 + (reach - y)**2
-        if dist_sqr <= reach_sqr
-          alpha = 255.0 * (dist_sqr.to_f / reach_sqr.to_f) #dist_sqr.to_f ** (-dist_sqr.to_f / (reach_sqr.to_f + dist_sqr.to_f)) #(dist_sqr.to_f / reach_sqr.to_f)
-          alpha = 255.0 - alpha 
-          color.alpha = alpha
-          alpha_color = color
-          canvas.set_pixel(x, y, alpha_color)
-        end
+  def update_layer(layer, stack)
+    if stack[0] != nil
+      layer.each do |alpha_layer|
+        alpha_layer.clear
+        layer.opacity = $light.gray
       end
     end
-    return canvas
+    while(stack_item = stack.pop)
+      #alpha_layer.draw_blue_ambiance(@blue_layer.bitmap)
+      #alpha_.draw_blue_beam(@blue_beam_layer.bitmap)
+      layer.each do |alpha_layer|
+        stack_item.draw(alpha_layer)
+      end
+    end
   end
 =end
 end
