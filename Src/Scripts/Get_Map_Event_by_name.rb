7da1a922a8243f750c4618dae9c0aa3ec04f5eb2
gegerlan <gegerlan2@hotmail.com>
2011-08-20 18:24:39 +0100

    Added the ability to call a map event by name call_event_by_name(event_name)
    Interpreter
      call_event(event[, event[, event ...]]])
        Execute one or more RPG::Game_Event events.
        The pages (command list) are taken
        based on RPGMaker XP's standard
        behaviour, and thus require that
        conditions are met, and the page
        furthest to the right is used.
    
      call_event_by_name(event_name)
        Run an event on the current map with the
        given name.
        Case-sensitive.
    
    Game_Event
      name
        returns the name of the event,
        set in the editor.
    
    call_event_by_name uses @@__map_events__ which
    indexes all the names of all events on the map.
    @@__map_events__ will only index the events on
    the map if call_event_by_name has been called.

---
+++ Src/Scripts/Get_Map_Event_by_name.rb
@@ -0,0 +1,32 @@
+class Game_Event < Game_Character
+  def name
+    return event.name
+  end
+end
+class Interpreter
+  def call_event_by_name(event_name)
+    @@__map_events__ ||= {}
+    if @@__map_events__[:map_id] != $game_map.map_id
+      @@__map_events__.clear
+      @@__map_events__[:map_id] = $game_map.map_id
+      $game_map.events.each do |index, map_event|
+        (@@__map_events__[map_event.name] ||= []) << map_event
+      end
+    end
+    return call_event(*@@__map_events__[event_name])
+  end
+  def call_event(*events)
+    p events
+    list = []
+    for event in events
+      list += event.list if event.is_a?(Game_Event)
+    end
+    if list.length != 0
+      # Make child interpreter
+      @child_interpreter = Interpreter.new(@depth + 1)
+      @child_interpreter.setup(list, @event_id)
+    end
+    # Continue
+    return true
+  end
+end
