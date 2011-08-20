a2305666e51a0ccf3efa0c69f079b3ffe93e6cec
gegerlan <gegerlan2@hotmail.com>
2011-08-21 00:52:17 +0100

    Added a test BR to the first junction after exiting Dali.
    Added appropriate skill. Skill calls CE which destroys Garnet's clothes, or if already nude; tries to execute a manager event on the map.
    Added skill use to Goblin. Will trigger more often if nude.

--- Src/Scripts/Get_Map_Event_by_name.rb
+++ Src/Scripts/Get_Map_Event_by_name.rb
@@ -5,10 +5,11 @@ class Game_Event < Game_Character
 end
 class Interpreter
   def call_event_by_name(event_name)
-    @@__map_events__ ||= {}
-    if @@__map_events__[:map_id] != $game_map.map_id
+    @@__map_events__ ||= {} # the maps events indexed by name
+    if @@__map_events__[:map_id] != $game_map.map_id # create a new index in non exsist for the map
       @@__map_events__.clear
       @@__map_events__[:map_id] = $game_map.map_id
+      # Index map events based on name (note that names doesn't have to be unique for an event)
       $game_map.events.each do |index, map_event|
         (@@__map_events__[map_event.name] ||= []) << map_event
       end
@@ -16,7 +17,6 @@ class Interpreter
     return call_event(*@@__map_events__[event_name])
   end
   def call_event(*events)
-    p events
     list = []
     for event in events
       list += event.list if event.is_a?(Game_Event)
@@ -26,7 +26,6 @@ class Interpreter
       @child_interpreter = Interpreter.new(@depth + 1)
       @child_interpreter.setup(list, @event_id)
     end
-    # Continue
     return true
   end
 end
