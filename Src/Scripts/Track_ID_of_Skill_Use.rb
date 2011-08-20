8bb0b77b9d312f0363a3dbdbe8205d700aab7984
gegerlan <gegerlan2@hotmail.com>
2011-08-20 18:17:51 +0100

    Calling a CE from a skill, item, etc. will now carry over the event/actor id to the CE.
    Instead of setting the event_id to 0 (Map), items (and other similar ways) will pass the event id of the object that requested the CE to be called.

---
+++ Src/Scripts/Track_ID_of_Skill_Use.rb
@@ -0,0 +1,17 @@
+class Map_Battler < Game_Character
+  #----------------------------------------------------------------------------
+  # common_event_call
+  #  object - the skill of item
+  #  callee_id - the id of the caster (map_id for enemy)
+  #  Runs a common event if supported by the object.
+  #----------------------------------------------------------------------------
+  def common_event_call(object, callee_id = id || 0)
+    # if object calls common event
+    if object.common_event_id > 0
+      # temporary variable
+      common_event = $data_common_events[object.common_event_id]
+      # setup common event execution
+      $game_system.map_interpreter.setup(common_event.list, callee_id)
+    end
+  end
+end
