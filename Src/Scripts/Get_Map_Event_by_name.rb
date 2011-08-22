74d46508742533cb54c8fb5879d77200c584e033
gegerlan <gegerlan2@hotmail.com>
2011-08-22 01:53:50 +0100

    Interpreter   call_common_events_by_name(event_name) -calls all common events with the given name   get_common_events - retrieves all common events with the given name (calls index if no index is found)   index_common_events - creates an index of all common events based on their name   get_event_by_name -> get_events_by_name - changed to signal that an array is given (rather than one event)   get_event_by_index - as an alternative to using names   call_event - now get's the page of the event and relies on call_page to pass it on for interpretation   call_page - given one or more pages will now retrieve the commands from the page and pass it on to call_command   call_command - set's one or more commands as the workload of the interpreter Game_Event   page - the current page is now publically readable   condition_met?(page)- if the condtions ofthe page are met (set in editor)   get_page_by_index(index, meet_conditions) - gets the page at the provided index, does checks if conditions are met if requested (TRUE)   get_page_by_name(page_name, meet_conditions) - gets the pages that has a comment with the format "name:page_name" e.g. "name:my_page" that has the passed name. Checks conditions if requested (TRUE)   index_page_names- indexes the name of the pages. Checks the 6 first commands of each page by default for a name. This can be modified by altering the const INDEX_DEPTH_MAX.
    Events overwriting the knockout CE are now required to be named "Knockout Manager"
    Changed the name Knockout -> Knockout Manager on several maps
    
    When the rape skill is executed, it will now first run CE45.
    If player is nude:
      if no event named "Knockout Manager" exists on map:
        run CE300 - Default Rape Manager
      end
    end
    
    CE300 in turn checks the name of the enemy and looks for events on the map or as a CE. E.g. for a goblin:
    if no event named "Rape:Goblin":
      find and run common events named "Rape:Goblin"
    end
    
    Rape:Goblin (CE303) in turn plays out the scene, as if it were an event on a map.
    
    Example of overwritten behaviour can be found on map 33 - Bramble Woods _junction [KTS]
    
    
    Fixed issue with persona showing up when it shouldn't (due to using switches to set visible state in update persona 2)
      Hide Persona -> Switch 127 = on
      Show Persona -> Switch 127 = off

--- Src/Scripts/Get_Map_Event_by_name.rb
+++ Src/Scripts/Get_Map_Event_by_name.rb
@@ -8,23 +8,46 @@
   Names can be shared by multiple events, all will be executed when called.
   
 =end
-# .name : returns the event name set in the editor
 class Game_Event < Game_Character
   def name
     return event.name
   end
 end
 class Interpreter
-  def call_event_by_name(event_name)
-    return call_event(*get_event_by_name(event_name))
+  def call_common_events_by_name(event_name)
+    # a common event is a page
+    return call_page(*get_common_events_by_name(event_name))
+  end
+  def get_common_events_by_name(event_name)
+    @@__common_events__ ||= {}
+    if @@__common_events__.empty?
+      index_common_events
+    end
+    return @@__common_events__[event_name.downcase.strip]
+  end
+  def index_common_events
+    @@__common_events__ = {}
+    for common_event in $data_common_events do
+      common_event_name = common_event.name if common_event != nil
+      if common_event_name != nil and not common_event_name.empty?
+        (@@__common_events__[common_event_name.downcase.strip] ||= []) << common_event
+      end
+    end
+  end
+  def call_events_by_name(event_name)
+    return call_event(*get_events_by_name(event_name))
   end
   # Get events sharing a name
-  def get_event_by_name(event_name)
+  def get_events_by_name(event_name)
     @@__map_events__ ||= {} # the maps events indexed by name
     if @@__map_events__[:map_id] != $game_map.map_id # create a new index in non exsist for the map
       index_events
     end
     return @@__map_events__[event_name.downcase]
+  end
+  # Get an event at the given index
+  def get_event_by_index(index)
+    return game_map.events[index]
   end
   def index_events
     @@__map_events__.clear
@@ -36,16 +59,108 @@ class Interpreter
   end
   # execute event passed
   def call_event(*events)
-    list = []
+    pages = []
     for event in events
-      list += event.list if event.is_a?(Game_Event)
+      pages << event.page if event != nil
     end
-    if list.length != 0
-      # Make child interpreter (i.e. make it running under the current interpreter)
-      @child_interpreter = Interpreter.new(@depth + 1)
-      @child_interpreter.setup(list, @event_id)
-      return true
+    return call_page(*pages)
+  end
+  # execute page passed
+  def call_page(*pages)
+    list = []
+    for page in pages
+      list += page.list if page != nil
     end
-    return false
+    return call_command(*list)
+  end
+  # execute list passed
+  def call_command(*commands)
+    return false if commands.empty?
+    # Make child interpreter (i.e. make it running under the current interpreter)
+    @child_interpreter = Interpreter.new(@depth + 1)
+    @child_interpreter.setup(commands, @event_id)
+    return true
+  end
+end
+class Game_Event
+  attr_reader :page
+  # check if the condtions for the page are met
+  def condition_met?(page)
+    c = page.condition
+    # Switch 1 condition confirmation
+    if c.switch1_valid
+      if $game_switches[c.switch1_id] == false
+        return false
+      end
+    end
+    # Switch 2 condition confirmation
+    if c.switch2_valid
+      if $game_switches[c.switch2_id] == false
+        return false
+      end
+    end
+    # Variable condition confirmation
+    if c.variable_valid
+      if $game_variables[c.variable_id] < c.variable_value
+        return false
+      end
+    end
+    # Self switch condition confirmation
+    if c.self_switch_valid
+      key = [@map_id, @event.id, c.self_switch_ch]
+      if $game_self_switches[key] != true
+        return false
+      end
+    end
+    return true
+  end
+  # get the page at the given index
+  def get_page_by_index(index, meet_conditions)
+    return @page if index < 1
+    return @event.pages[index] if @event.pages[index] != nil &&
+      (not meet_conditions or (meet_conditions && condition_met?(@event.pages[index])))
+  end
+  # Get an array of pages having the name provided  
+  def get_pages_by_name(page_name, meet_conditions)
+    if @page_names == nil
+      index_page_names
+    end
+    pages = @page_names[page_name.downcase]
+    if meet_conditions
+      pages.keep_if {  |v|
+        condition_met?(v)
+      }
+    end
+    return pages
+  end
+  INDEX_DEPTH_MAX = 6 # How many commands are checked on each list for a name
+  # Index pages that has the comment command with name:
+  def index_page_names
+    @page_names = {}
+    for page in pages.reverse
+      depth = 0
+      for event_command in page.list
+        if event_command.code == 108 || event_command.code == 408 # Comment command
+          parameters = event_command.parameters
+          parameter_matches = []
+          for parameter in parameters
+            parameter_matches += parameter.scan(/^name:([^$#]+)/im)
+          end
+          for command_matches in parameter_matches
+            for name in command_matches
+              name.strip! # Remove starting and trailing white space
+              name.downcase! # Make name lowercase
+              if not name.empty?
+                (@page_names[name] ||= []) << page
+              end
+            end
+          end
+        end
+        depth += 1
+        if depth > INDEX_DEPTH_MAX
+          break
+        end
+      end
+    end
   end
 end
