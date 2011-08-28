df5245f7746d99c8088ace0a1259eea73c710f56
gegerlan <gegerlan2@hotmail.com>
2011-08-28 15:22:52 +0100

    State tracker now verifies that the name passed and converted is a valid constant in ruby.
    Added regex that checks that the name before passing it to the cost lookup method.
    regex used: [A-Z_][A-Za-z_0-9]*
      1. [A-Z_] Starts with capital letter (A-Z) or underscore (_)
      2. [A-Za-z_0-9]* If followed by one or more characters (*), it may be one or more letters (A-Z or a-z), underscores (_) or digits (0-9)

--- Src/Scripts/State_tracker.rb
+++ Src/Scripts/State_tracker.rb
@@ -16,7 +16,7 @@
     
 =end
 module StateTracker
-  # State name (uppercase) = Switch ID
+  # State name (upper_case) = Switch ID
   KNOCKOUT = 240
 end
 class Game_Battler
@@ -26,10 +26,15 @@ class Game_Battler
       # state_name needs to be uppercase since constants may only be uppercase
       state = $data_states[state_id]
       if state != nil
-        # makes spaces _ to have valid constant names.
+        # makes state name uppercase, and spaces -> _ to have valid constant names.
         state_name = state.name.upcase.gsub(/[\s\t]/,"_")
-        if StateTracker.const_defined?(state_name)
-          $game_switches[StateTracker.const_get(state_name)] = callee.state?(state_id)
+        # verify that the state_name is a valid const (starts with capital 
+        # letter or underscore by itself, or followed by one or more letters 
+        # (up/down case) digits or underscores.
+        if state_name =~ /[A-Z_][A-Za-z_0-9]*/
+          if StateTracker.const_defined?(state_name)
+            $game_switches[StateTracker.const_get(state_name)] = callee.state?(state_id)
+          end
         end
       end
     end
