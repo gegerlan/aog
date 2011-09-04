cfb89da8ef25915a1d977e73b9beaa292d64ff15
gegerlan <gegerlan2@hotmail.com>
2011-09-04 18:14:23 +0100

    Added the basic script for getting the persona layers.

--- Src/Scripts/Persona.rb
+++ Src/Scripts/Persona.rb
@@ -1,230 +1,265 @@
-# TODO:
-#  * Fix fugly Dog class fix
-#  * Animation of fading
-#  * Easy hook for persona (maybe)
-class Persona < Sprite
-  attr_reader :actor
-  def initialize(viewport = nil)
-    super(viewport)
-    self.bitmap = Bitmap.new(640,480)
-    @visible = true
-    self.z = 9999
-    @actor = $game_party.actors[0]
-  end
-  def self.switch 
-    return 128
-  end
-  def self.duration
-    return 10
-  end
-  def self.visible_opacity
-    return 255
-  end
-  def self.transparent_opacity
-    return 50
-  end
-  def visibility=(visibility)
-    if @visible != visibility
-      @visible = !!visibility
-      self.bitmap.clear if !is_visible?
-      #self.update_persona
-      @old_armor_set = nil if is_visible? #our old picture was nothing, do an update even if equipment is the same!
-      update if is_visible?
-    end
-  end
-  def is_visible?
-    return @visible
-  end
-  def set_transparent(fade)
-    self.opacity = !!fade ? 50 : 255
-  end
-  def is_transparent?
-    return self.opacity != 255
+class Persona
+  SCREEN_WIDTH  = 640
+  SCREEN_HEIGHT = 480
+  
+  # Center position for each layer (relative to the viewport center)
+  OFFSET = {
+    :mouth =>  { :x => 53, :y => 84,  :zoom => 0.9},
+    :eyes  =>  { :x => 51, :y => 59, :zoom => 0.9},
+    :armor3 => { :x => 58, :y => 211, :zoom => 1},
+  }
+  # Z-index of item. The higher the number, the more to the front
+  ORDER = {
+    :base   => 5,
+    :mouth  => 10,
+    :eyes   => 10,
+    :armor1 => 20,
+    :armor2 => 50,
+    :armor3 => 30,
+    :armor4 => 40,
+  }
+  
+  def initialize(actor = $game_actors[1])
+    @viewport = Viewport.new(0,0,640,480) # Create an empty viewport
+    @actor = actor
+    @layers = {}
+    update
   end
   def update
-    super
-    if !is_visible?
-      return
+    if @layers.empty?
+      setup_actor
     end
-    actor_name = @actor.name
-    actor_class_name = $data_classes[@actor.class_id].name
-    
-    actor_armors = [] # Organize for draw order
-    actor_armors << @actor.armor3 # Body armor (bottom)
-    actor_armors << @actor.armor2 # Helmet
-    actor_armors << @actor.armor4 # Accessories
-    actor_armors << @actor.armor1 # Shield (top)
-    
-    armor_set = actor_armors.map { |armor| 
-      [armor.name, armor.condition] if armor != nil
-    }
-    
-    weapon_set = [@actor.weapon].map{ |weapon| 
-      [weapon.name, weapon.condition] if weapon != nil
-    }
-
-    shy = @actor.is_shy?
-    cuffs = @actor.is_cuffed?    
-    
-    base_modifiers = []
-    base_modifiers << "shy" if shy
-    base_modifiers << "cuffs" if cuffs
-    
-    #todo: make a proper hash
-    if weapon_set != @old_weapon_set || 
-        armor_set != @old_armor_set || 
-        actor_class_name != @old_actor_class_name || 
-        actor_name != @old_actor_name ||
-        base_modifiers != @old_base_modifiers
-      self.bitmap.clear
-      
-      layers = []
-      layers << [actor_name, actor_class_name].join(" ")
-
-      layers += weapon_set if actor_class_name != "Dog" #FIXME!
-
-      layers += armor_set if actor_class_name != "Dog" #FIXME!
-      
-
-      layers.each do |layer|
-        next if layer == nil
-        
-        modifiers = base_modifiers.clone
-        
-        if layer.is_a?(Array)
-          layer_name, layer_condition = layer
-        elsif layer.is_a?(String)
-          layer_name = layer
-        elsif layer.is_a?(Condition_Item)
-          layer_name = layer.name
-          layer_condition = layer.condition
-        else
-          return false
-        end
-        
-        # These are listed with the worst first, so when the files are
-        # searched for, the worst condition possible will come on top
-        # of the return list.
-        if layer_condition != nil
-          modifiers << "broken" if layer_condition < 25
-          modifiers << "torn" if layer_condition < 50
-          modifiers << "chipped" if layer_condition < 75
-        end
-        
-        # Try getting actor name + actor class + layer image
-        picture_path = get_picture_name([actor_name, actor_class_name, layer_name].join(" "), modifiers)
-        # Try getting actor name + layer image
-        picture_path = get_picture_name([actor_name, layer_name].join(" "), modifiers) if picture_path == nil
-        # Try getting layer image
-        picture_path = get_picture_name(layer_name, modifiers) if picture_path == nil
-        
-        if picture_path != nil
-          draw(picture_path)
-        else
-          # print "Unable to find %s" % layer if $DEBUG
-        end
-      end
-      @old_armor_set = armor_set
-      @old_weapon_set = weapon_set
-      @old_actor_class_name = actor_class_name
-      @old_actor_name = actor_name
-      @old_base_modifiers = base_modifiers
+    @layers.each do |index, layer|
+      layer.update
     end
-  end
-  def draw(image_path)
-    begin
-      bitmap = RPG::Cache.picture(image_path)
-      rect = Rect.new(0,0,bitmap.width,bitmap.height)
-      self.bitmap.blt(0,0,bitmap,rect)
-    rescue Exception => e
-      print e if $DEBUG
-    end
-  end
-  def actor=(actor)
-    if @actor != actor
-      @actor = actor
-      update
-    end
+    @viewport.update
   end
   def dispose
     super
-    @old_armor_set.clear if @old_armor_set != nil
-    @old_weapon_set.clear if @old_weapon_set != nil
-    @old_base_modifiers.clear if @old_base_modifiers != nil
+  end
+  
+  # Setup the persona for the given actor
+  def setup_actor(actor = @actor)
+    @actor = actor
+    setup_layers
+    setup_viewport
+  end
+  def setup_layers
+    # Get the layers
+    @layers[:base] = get_base_layer
+    @layers[:weapon] = get_weapon_layer
+    @layers.merge!(get_armor_layers)
+    @layers.merge!(get_face_layers)
+    
+    # Configure layers
+    @layers.each do |index, image|
+      if image_offset = OFFSET[index]
+        @layers[index].x = image_offset[:x]
+        @layers[index].y = image_offset[:y]
+        @layers[index].zoom_y = image_offset[:zoom]
+        @layers[index].zoom_x = image_offset[:zoom]
+      end
+      if image_depth = ORDER[index]
+        @layers[index].z = image_depth
+      end
+    end
+  end
+
+  def get_base_layer
+    proc_value = Proc.new { |callee|
+      @actor.class_id
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      class_name = $data_classes[value].name.strip
+      "Graphics/Persona/#{actor_name}/Base/#{class_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_armor_layers
+    return {
+      :armor1 => get_armor_layer(:armor1_id),
+      :armor2 => get_armor_layer(:armor2_id),
+      :armor3 => get_armor_layer(:armor3_id),
+      :armor4 => get_armor_layer(:armor4_id)
+    }
+  end
+  def get_armor_layer(sym)
+    proc_value = Proc.new { |callee|
+      @actor.send(sym)
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      if value != 0
+        armor_name = $data_armors[value].name.strip
+        "Graphics/Persona/#{actor_name}/Equipment/Armor/#{armor_name}"
+      else
+        ""
+      end
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_weapon_layer
+    proc_value = Proc.new { |callee|
+      @actor.send(:weapon_id)
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      if value != 0
+        weapon_name = $data_weapons[value].name.strip
+        "Graphics/Persona/#{actor_name}/Equipment/Weapon/#{weapon_name}"
+      else
+        ""
+      end
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_face_layers
+    return {
+      :eyes => get_eyes_layer,
+      :tears => get_tears_layer,
+      :mouth => get_mouth_layer,
+      :drool => get_drool_layer,
+      :stains => get_stains_layer,
+    }
+  end
+  def get_eyes_layer
+    proc_value = Proc.new { |callee|
+      1
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      eyes_name = "#{value}".strip
+      "Graphics/Persona/#{actor_name}/Face/Eyes/#{eyes_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_mouth_layer
+    proc_value = Proc.new { |callee|
+      1
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      mouth_name = "#{value}".strip
+      "Graphics/Persona/#{actor_name}/Face/Mouth/#{mouth_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_tears_layer
+    proc_value = Proc.new { |callee|
+      1
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      tears_name = "#{value}".strip
+      "Graphics/Persona/#{actor_name}/Face/Tears/#{tears_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_drool_layer
+    proc_value = Proc.new { |callee|
+      1
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      drool_name = "#{value}".strip
+      "Graphics/Persona/#{actor_name}/Face/Tears/#{drool_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_stains_layer
+    proc_value = Proc.new { |callee|
+      1
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      stains_name = "#{value}".strip
+      "Graphics/Persona/#{actor_name}/Face/Tears/#{stains_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def setup_viewport
+    base_width, base_height = @layers[:base].width, @layers[:base].height
+    
+    viewport_width = base_width
+    viewport_height = base_height
+    viewport_x = SCREEN_WIDTH - viewport_width
+    viewport_y = SCREEN_HEIGHT - viewport_height
+    
+    @viewport.rect = Rect.new(viewport_x, viewport_y, viewport_width, viewport_height)
+    
+    @viewport.z = 100
+
   end
 end
-# Extending the default spriteset class for getting the scene map viewport1
-class Spriteset_Map
-  attr_reader :viewport1
-  attr_reader :picture_sprites
+class Persona_Picture < Sprite
+  def initialize(viewport, proc_value, proc_value_process)
+    super(viewport)
+    @proc_value = proc_value
+    @proc_value_process = proc_value_process
+    update
+  end
+  def width
+    return self.bitmap.width
+  end
+  def height
+    return self.bitmap.height
+  end
+  def setup_image(picture)
+    new_image = self.bitmap == nil
+    self.bitmap.clear if not new_image 
+    begin
+      if picture == "" # if no image provided
+        self.visible = false
+        return
+      end
+      
+      self.bitmap = Bitmap.new(picture)#RPG::Cache::load_bitmap(picture)
+      
+      # Center origin on image (the point it uses to set the location of the image)
+      self.ox = self.bitmap.width  / 2 
+      self.oy = self.bitmap.height / 2 
+
+      # Center position of image if we haven't previously given it a location
+      if new_image
+        self.x = self.bitmap.width   / 2
+        self.y = self.bitmap.height  / 2
+      end
+      
+      # Show image
+      self.visible = true
+    rescue
+      # Error
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Dispose
+  #--------------------------------------------------------------------------
+  def dispose
+    if self.bitmap != nil
+      self.bitmap.dispose
+    end
+    super
+  end
+  #--------------------------------------------------------------------------
+  # * Frame Update
+  #--------------------------------------------------------------------------
+  def update
+    super
+    update_value = @proc_value.call(self)
+    if @old_proc_value != update_value
+      setup_image(@proc_value_process.call(update_value, self))
+    end
+    @old_proc_value = update_value
+  end
 end
-# Present the persona as a map is loaded
 class Scene_Map
   alias persona_update update
   def update
+    if @persona == nil
+      @persona = Persona.new
+    end
+    @persona.update
     persona_update
-    if $persona == nil
-      add_persona
-    end
-  end
-  alias persona_main main  
-  def main
-    persona_main
-    $persona.dispose
-    $persona = nil
-  end
-  alias persona_transfer transfer_player
-  def transfer_player
-    persona_transfer
-    add_persona
-  end
-  def add_persona
-    $persona = Persona.new(@spriteset.viewport1)
-    @spriteset.picture_sprites.push($persona)
-    $persona.set_transparent($game_player.screen_x > 450)
-  end
-end
-# Add a persona to the equipment menu
-class Scene_Equip
-  alias persona_main main
-  def main
-    persona_main
-    $persona.dispose
-    $persona = nil
-  end
-  alias persona_update update
-  def update
-    persona_update
-    if $persona == nil
-      add_persona
-    end
-    $persona.update
-  end
-  def add_persona
-    $persona = Persona.new
-    $persona.actor = @actor
-  end
-end
-module BlizzABS
-  class Controller
-    alias persona_check_event_trigger_here check_event_trigger_here
-    # Change the persona to transparent if the actor is over 450
-    def check_event_trigger_here(triggers)
-      persona_check_event_trigger_here(triggers)
-      $persona.set_transparent($game_player.screen_x > 450) if $persona != nil
-    end
-  end
-end
-class Game_Actor
-  # Is the actor shy? Only if no body armor, nudity is not OK and perversion is less than 5
-  def is_shy?
-     return is_nude? && $game_switches[89] == FALSE && $game_variables[49] < 5
-  end
-  def is_nude?
-    return armor3_id == 0
-  end
-  # Is the actor cuffed? Only if the weapon worn is 33.
-  def is_cuffed?
-    return weapon_id == 33
   end
 end
