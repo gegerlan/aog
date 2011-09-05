4bf76b04a75a2d7c52a4b5ca416b5302840870cc
gegerlan <gegerlan2@hotmail.com>
2011-09-05 19:30:40 +0100

    Added arms to the persona system. Tweaked graphics rendering routine for the persona pictures.

--- Src/Scripts/Persona.rb
+++ Src/Scripts/Persona.rb
@@ -2,29 +2,34 @@ class Persona
   SCREEN_WIDTH  = 640
   SCREEN_HEIGHT = 480
   
+  # WEAPONS
+  CUFFS = 33
+  
   # Center position for each layer (relative to the viewport center)
   OFFSET = {
     :mouth =>  { :x => 53, :y => 84,  :zoom => 0.9},
     :eyes  =>  { :x => 51, :y => 59, :zoom => 0.9},
-    :armor3 => { :x => 58, :y => 211, :zoom => 1},
+    :armor3 => { :x => 63, :y => 211, :zoom => 1},
     :blush => { :x => 52, :y => 75, :zoom => 0.9},
     :tears => { :x => 52, :y => 72, :zoom => 0.9},
   }
   # Z-index of item. The higher the number, the more to the front
-  ORDER = { # note, must be > 0
+  ORDER = {
     :hair_back => 1,
-    :base   => 5,
-    :blush  => 10,
-    :drool  => 15,
-    :mouth  => 20,
-    :eyes   => 25,
-    :tears   => 27,
-    :stains  => 30,
-    :armor1 => 70,
-    :hair_front => 55,
-    :armor2 => 60,
-    :armor3 => 40,
-    :armor4 => 50,
+    :body      => 5,
+    :arms      => 7,
+    :armor3_arms=>8,
+    :blush     => 10,
+    :drool     => 15,
+    :mouth     => 20,
+    :eyes      => 25,
+    :tears     => 27,
+    :stains    => 30,
+    :armor3    => 40,
+    :armor4    => 50,
+    :hair_front=> 55,
+    :armor2    => 60,
+    :armor1    => 70,
   }
   
   def initialize(actor = $game_actors[1])
@@ -53,8 +58,9 @@ class Persona
     setup_viewport
   end
   def setup_layers
+    @layers = {}
     # Get the layers
-    @layers[:base] = get_base_layer
+    @layers.merge!(get_base_layers)
     @layers.merge!(get_hair_layers)
     @layers[:weapon] = get_weapon_layer
     @layers.merge!(get_armor_layers)
@@ -74,14 +80,32 @@ class Persona
     end
   end
 
-  def get_base_layer
+  def get_base_layers
+    return {
+      :body => get_body_layer,
+      :arms => get_arms_layer,
+    }
+  end
+  def get_body_layer
     proc_value = Proc.new { |callee|
       @actor.class_id
     }
     proc_value_process = Proc.new { |value, callee|
       actor_name = @actor.name.strip
       class_name = $data_classes[value].name.strip
-      "Graphics/Persona/#{actor_name}/Base/#{class_name}"
+      "Graphics/Persona/#{actor_name}/Body/#{class_name}"
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_arms_layer
+    proc_value = Proc.new { |callee|
+      #@actor.state?(STATE_CUFFED) ? 1 : 0
+      @actor.weapon.id == CUFFS ? 1 : 0
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      arms_name = value
+      "Graphics/Persona/#{actor_name}/Body/Arms/#{arms_name}"
     }
     return Persona_Picture.new(@viewport, proc_value, proc_value_process)
   end
@@ -92,12 +116,12 @@ class Persona
     proc_value_process_front = Proc.new { |value, callee|
       actor_name = @actor.name.strip
       hair_name = "#{value}"
-      "Graphics/Persona/#{actor_name}/Base/Hair/Front/#{hair_name}"
+      "Graphics/Persona/#{actor_name}/Body/Hair/Front/#{hair_name}"
     }
     proc_value_process_back = Proc.new { |value, callee|
       actor_name = @actor.name.strip
       hair_name = "#{value}"
-      "Graphics/Persona/#{actor_name}/Base/Hair/Back/#{hair_name}"
+      "Graphics/Persona/#{actor_name}/Body/Hair/Back/#{hair_name}"
     }
     
     return {
@@ -111,7 +135,7 @@ class Persona
       :armor1 => get_armor_layer(:armor1_id),
       :armor2 => get_armor_layer(:armor2_id),
       :armor3 => get_armor_layer(:armor3_id),
-      :armor4 => get_armor_layer(:armor4_id)
+      :armor4 => get_armor_layer(:armor4_id),
     }
   end
   def get_armor_layer(sym)
@@ -123,6 +147,23 @@ class Persona
       if value != 0
         armor_name = $data_armors[value].name.strip
         "Graphics/Persona/#{actor_name}/Equipment/Armor/#{armor_name}"
+      else
+        ""
+      end
+    }
+    return Persona_Picture.new(@viewport, proc_value, proc_value_process)
+  end
+  def get_armor3_arms_layer(sym)
+    proc_value = Proc.new { |callee|
+      @actor.weapon.id == CUFFS ? 1 : 0 #@actor.state?(STATE_CUFFED) ?  1 : 0
+    }
+    proc_value_process = Proc.new { |value, callee|
+      actor_name = @actor.name.strip
+      armor3_id = @actor.armor3_id
+      if armor3_id != 0
+        armor_name = $data_armors[armor3_id].name.strip
+        arms_name = value
+        "Graphics/Persona/#{actor_name}/Equipment/Armor/#{armor_name}/arms_name"
       else
         ""
       end
@@ -178,7 +219,7 @@ class Persona
   end
   def get_tears_layer
     proc_value = Proc.new { |callee|
-      1
+      0
     }
     proc_value_process = Proc.new { |value, callee|
       actor_name = @actor.name.strip
@@ -189,7 +230,7 @@ class Persona
   end
   def get_blush_layer
     proc_value = Proc.new { |callee|
-      1
+      0
     }
     proc_value_process = Proc.new { |value, callee|
       actor_name = @actor.name.strip
@@ -221,7 +262,7 @@ class Persona
     return Persona_Picture.new(@viewport, proc_value, proc_value_process)
   end
   def setup_viewport
-    base_width, base_height = @layers[:base].width, @layers[:base].height
+    base_width, base_height = @layers[:body].width, @layers[:body].height
     
     viewport_width = base_width
     viewport_height = base_height
@@ -231,7 +272,6 @@ class Persona
     @viewport.rect = Rect.new(viewport_x, viewport_y, viewport_width, viewport_height)
     
     @viewport.z = 100
-
   end
 end
 class Persona_Picture < Sprite
@@ -248,31 +288,32 @@ class Persona_Picture < Sprite
     return self.bitmap.height
   end
   def setup_image(picture)
+    return if @old_picture == picture
     new_image = self.bitmap == nil
-    self.bitmap.clear if not new_image 
-    begin
-      if picture == "" # if no image provided
-        self.visible = false
-        return
+    self.bitmap.clear if not new_image
+    if picture == "" # if no image provided
+      self.visible = false
+    else
+      begin  
+        self.bitmap = Bitmap.new(picture)#RPG::Cache::load_bitmap(picture)
+        
+        # Center origin on image (the point it uses to set the location of the image)
+        self.ox = self.bitmap.width  / 2 
+        self.oy = self.bitmap.height / 2 
+  
+        # Center position of image if we haven't previously given it a location
+        if new_image
+          self.x = self.bitmap.width   / 2
+          self.y = self.bitmap.height  / 2
+          # Show image
+          self.visible = true
+        end
+        
+      rescue
+        # Error
       end
-      
-      self.bitmap = Bitmap.new(picture)#RPG::Cache::load_bitmap(picture)
-      
-      # Center origin on image (the point it uses to set the location of the image)
-      self.ox = self.bitmap.width  / 2 
-      self.oy = self.bitmap.height / 2 
-
-      # Center position of image if we haven't previously given it a location
-      if new_image
-        self.x = self.bitmap.width   / 2
-        self.y = self.bitmap.height  / 2
-      end
-      
-      # Show image
-      self.visible = true
-    rescue
-      # Error
     end
+    @old_picture = picture
   end
   #--------------------------------------------------------------------------
   # * Dispose
