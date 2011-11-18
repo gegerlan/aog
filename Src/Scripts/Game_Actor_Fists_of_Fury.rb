db04633a8de0efc6554a23730549b147b46f5cab
gegerlan <gegerlan2@hotmail.com>
2011-11-18 00:49:39 +0000

    Added effect for hitting things without a weapon. If @weapon_id is 0 (i.e. no weapon) will now provide the animation2_id (target hit animation) to 4, as set by FIST_HIT_ANIMATION_ID for Game_Actor.

--- Src/Scripts/Game_Actor_Fists_of_Fury.rb
+++ Src/Scripts/Game_Actor_Fists_of_Fury.rb
@@ -1,7 +1,11 @@
 class Game_Actor < Game_Battler
+  FIST_HIT_ANIMATION_ID = 4
+  alias no_fists_base_atk base_atk
   def base_atk
-    weapon = $data_weapons[@weapon_id]
-    atk = weapon != nil ? weapon.atk : base_str * 2
-    return atk
+    return @weapon_id == 0 ? base_str * 2 : no_fists_base_atk
+  end
+  alias no_fists_animation2_id animation2_id
+  def animation2_id
+    return @weapon_id == 0 ? FIST_HIT_ANIMATION_ID : no_fists_animation2_id
   end
 end
