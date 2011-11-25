c43cf5da3453671044911b517c763a7b97facd28
gegerlan <gegerlan2@hotmail.com>
2011-11-25 01:57:37 +0000

    Added fix for old save game to work with new perversion and arousal system.

---
+++ Src/Scripts/TEMP_Arousal_N_Perversion_savegame_fix.rb
@@ -0,0 +1,24 @@
+class Game_Player < Map_Actor
+  alias update_original_arousal_init update
+  def update
+    if @arousal_ticker == nil
+      @arousal_ticker = 0
+    end
+    update_original_arousal_init
+    alias update update_original_arousal_init
+  end
+end
+
+class Game_Actor < Game_Battler  
+  alias add_perversion_original_init add_perversion
+  def add_perversion(value, unique_id = 0, event_id = 0, map_id = $game_map.map_id)
+    if @perversion == nil
+      @perversion = {}
+      @perversion[[0,0,0]] = $game_variables[49]
+      $game_variables[49]  = sum_perversion
+    else
+      add_perversion_original_init(value, unique_id = 0, event_id = 0, map_id = $game_map.map_id)
+    end
+    alias add_perversion add_perversion_original_init
+  end
+end
