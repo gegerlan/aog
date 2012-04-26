e8ec7aa51073f1ae1aaf0f3a62bb5caddbd32192
omglolwtfbbq <sparesubject159753@hotmail.com>
2012-04-26 14:23:09 +0800

    Items - Added Pan shield and Pot helm 001 - Dali Locked Potts House unless night 009 - Garnet Upstairs Proxied the bed 004 - Potts House Put a Pan Shield and Pot Helm Allow Garnet to steal cash from Mrs. Potts (200 G) 035 - Bramble blocked south Junction Improved the layout of the roadblock slightly 146 - Sewer Added a bit more text here and there. Erase the soldiers once the boss is defeated. 147 - Orphanage Put in a choice to steal or not to steal

--- Src/Scripts/Main.rb
+++ Src/Scripts/Main.rb
@@ -1,28 +1,8 @@
-#==============================================================================
-# ** Main
-#------------------------------------------------------------------------------
-#  After defining each class, actual processing begins here.
-#==============================================================================
-
-begin
-  # Prepare for transition
-  $fontface = "Arial"  
-  $fontsize = 24   
-  Graphics.freeze
-  # Make scene object (title screen)
-  $scene = Scene_Title.new
-  # Call main method as long as $scene is effective
-  while $scene != nil
-    $scene.main
-  end
-  # Fade out
-  Graphics.transition(20)
-rescue Errno::ENOENT =>e
-  # Supplement Errno::ENOENT exception
-  # If unable to open file, display message and end
-  filename = $!.message.sub("No such file or directory - ", "")
-  File.open("error.log", File::WRONLY|File::APPEND|File::CREAT) do |fh|
-    fh << "#{Time.new.inspect}: #{e.backtrace}\n"
-  end
-  print("Unable to find file #{filename}.\nFull error message written to error.log")
+FILE_NAME = "..\\Game.bat"
+if File.exists?(FILE_NAME)
+  p "Unable to find files that should have been imported. Press OK to start #{File.basename(FILE_NAME)}."
+  system("start #{FILE_NAME}")
+else
+  p "Unable to find conversion tool #{File.basename(FILE_NAME)}. Verify that all content has been extracted, and press OK to exit application." 
 end
+exit
